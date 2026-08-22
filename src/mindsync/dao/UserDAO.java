6f44faccf52c5bbe30f52cd862b6804d48c52814
Lorsh07 <s2668@e-mirim.hs.kr>
2026-08-22 21:50:34 +0900

    Model 패키지 fin.

---
+++ src/mindsync/dao/UserDAO.java
@@ -0,0 +1,85 @@
+package mindsync.dao;
+
+import mindsync.db.DatabaseManager;
+import mindsync.model.Patient;
+import mindsync.model.Doctor;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class UserDAO {
+    public boolean registerPatient(Patient patient){
+        String sql = "INSERT INTO users (id, username, password_hash, name, role, region) " +
+                    "VALUES (?, ?, ?, ?, ?, ?)";
+
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, patient.getId());
+            pstmt.setString(2, patient.getUsername());
+            pstmt.setString(3, patient.getPasswordHash());
+            pstmt.setString(4, patient.getName());
+            pstmt.setString(5, patient.getRole());
+            pstmt.setString(6, patient.getRegion());
+
+            pstmt.executeUpdate();
+            return true;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public Patient loginPatient(String username, String passwordHash){
+        String sql = "SELECT * FROM users WHERE username = ? AND password_hash = ? AND role = '환자'";
+
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, username);
+            pstmt.setString(2, passwordHash);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                String id = rs.getString("id");
+                String name = rs.getString("name");
+                String region = rs.getString("region");
+                return new Patient(id, username, passwordHash, name, region);
+            }else {
+                return null;
+            }
+            } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+
+        }
+    }
+    public boolean registerDoctor(Doctor doctor) {
+        String sql = "INSERT INTO users (id, username, password_hash, name, role, region, license_number, specialty) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, doctor.getId());
+            pstmt.setString(2, doctor.getUsername());
+            pstmt.setString(3, doctor.getPasswordHash());
+            pstmt.setString(4, doctor.getName());
+            pstmt.setString(5, doctor.getRole());
+            pstmt.setString(6, doctor.getHospitalRegion());
+            pstmt.setString(7, doctor.getLicenseNumber());
+            pstmt.setString(8, doctor.getSpecialty());
+
+            pstmt.executeUpdate();
+            return true;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
